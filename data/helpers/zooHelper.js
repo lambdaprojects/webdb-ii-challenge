f5f620cd357a2e6fe946f4edd0ccbd1710f54e62
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-04 09:57:01 -0500

    Completed findById()

--- data/helpers/zooHelper.js
+++ data/helpers/zooHelper.js
@@ -1,9 +1,16 @@
 const db = require("../dbconfig.js");
 
 module.exports = {
-  find
+  find,
+  findById
 };
 
 function find() {
+  console.log(`:: THE QUERY IS :: ${db("zoos")}`);
   return db("zoos");
+}
+
+function findById(id) {
+  console.log(`:: THE QUERY IS :: ${db("zoos").where({ id })}`);
+  return db("zoos").where({ id });
 }
--- data/routers/zooRouter.js
+++ data/routers/zooRouter.js
@@ -8,7 +8,18 @@ router.get("/", async (req, res) => {
     res.status(200).json(animals);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ ErrorMessage: "Could not retrieve zoo animals" });
+    res.status(500).json({ ErrorMessage: "Could not retrieve zoo names" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const animal = await zooHelper.findById(req.params.id);
+    res.status(200).json(animal);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ ErrorMessage: "Could not retrieve the zoo name for that id" });
   }
 });
 
